0910850021f8145e3144469e4ac07cf922fec5a2
anhchienlyka <phamchien96nd@gmail.com>
2018-09-21 18:26:57 +0700

    update

---
+++ OnlineShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Data.Infrastructure
+{
+   public class DbFactory :Disposable,IDbFactory
+    {
+
+        private OnlineShopDBContext dbContext;
+
+        public OnlineShopDBContext Init()
+        {
+            return dbContext ?? (dbContext = new OnlineShopDBContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
