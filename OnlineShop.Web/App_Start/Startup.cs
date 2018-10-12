c9ead56b8c54448553cf1d237ceab2d06b0f7479
anhchienlyka <phamchien96nd@gmail.com>
2018-10-12 17:19:30 +0700

    commit MigrationData

--- OnlineShop.Web/App_Start/Startup.cs
+++ OnlineShop.Web/App_Start/Startup.cs
@@ -12,23 +12,24 @@ using System.Web.Http;
 using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
 using OnlineShop.Data;
-//using Microsoft.AspNet.Identity;
-//using OnlineShop.Mode.Models;
-//using Microsoft.Owin.Security;
-//using Microsoft.Owin.Security.DataProtection;
+using Microsoft.AspNet.Identity;
+using OnlineShop.Mode.Models;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataProtection;
 using System.Web;
 
 [assembly: OwinStartup(typeof(OnlineShop.Web.App_Start.Startup))]
 
 namespace OnlineShop.Web.App_Start
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-             ConfigAutofac(app);
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigAutofac(app);
+            ConfigureAuth(app);
         }
-
         private void ConfigAutofac(IAppBuilder app)
         {
             var builder = new ContainerBuilder();
@@ -42,11 +43,11 @@ namespace OnlineShop.Web.App_Start
             builder.RegisterType<OnlineShopDBContext>().AsSelf().InstancePerRequest();
 
             //Asp.net Identity
-            //builder.RegisterType<ApplicationUserStore>().As<IUserStore<ApplicationUser>>().InstancePerRequest();
-            //builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
-            //builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
-            //builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
-            //builder.Register(c => app.GetDataProtectionProvider()).InstancePerRequest();
+            builder.RegisterType<ApplicationUserStore>().As<IUserStore<ApplicationUser>>().InstancePerRequest();
+            builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
+            builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
+            builder.Register(c => app.GetDataProtectionProvider()).InstancePerRequest();
 
 
             // Repositories
