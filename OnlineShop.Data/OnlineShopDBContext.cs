ff6b191f51ac27dceb9355546956742c5d3812dd
anhchienlyka <phamchien96nd@gmail.com>
2018-10-12 17:07:53 +0700

    commit file Identityconfig-bai 15

--- OnlineShop.Data/OnlineShopDBContext.cs
+++ OnlineShop.Data/OnlineShopDBContext.cs
@@ -1,4 +1,5 @@
-﻿using OnlineShop.Mode.Models;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using OnlineShop.Mode.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -8,9 +9,9 @@ using System.Threading.Tasks;
 
 namespace OnlineShop.Data
 {
-  public  class OnlineShopDBContext :DbContext
+    public class OnlineShopDBContext : IdentityDbContext<ApplicationUser>
     {
-        public OnlineShopDBContext() :base("OnlineShopConnection")
+        public OnlineShopDBContext() : base("OnlineShopConnection")
         {
             Configuration.LazyLoadingEnabled = false;
         }
@@ -33,13 +34,21 @@ namespace OnlineShop.Data
         public DbSet<VisitorStatistic> VisitorStatistics { set; get; }
         public DbSet<Error> Errors { set; get; }
         public DbSet<Feedback> Feedbacks { set; get; }
-        
+        public DbSet<ApplicationGroup> ApplicationGroups { set; get; }
+        //public DbSet<ApplicationRole> ApplicationRoles { set; get; }
+        //public DbSet<ApplicationRoleGroup> ApplicationRoleGroups { set; get; }
+        //public DbSet<ApplicationUserGroup> ApplicationUserGroups { set; get; }
+
+        public static OnlineShopDBContext Create()
+        {
+            return new OnlineShopDBContext();
+        }
         protected override void OnModelCreating(DbModelBuilder builder)
         {
-            //builder.Entity<IdentityUserRole>().HasKey(i => new { i.UserId, i.RoleId }).ToTable("ApplicationUserRoles");
-            //builder.Entity<IdentityUserLogin>().HasKey(i => i.UserId).ToTable("ApplicationUserLogins");
-            //builder.Entity<IdentityRole>().ToTable("ApplicationRoles");
-            //builder.Entity<IdentityUserClaim>().HasKey(i => i.UserId).ToTable("ApplicationUserClaims");
+            builder.Entity<IdentityUserRole>().HasKey(i => new { i.UserId, i.RoleId }).ToTable("ApplicationUserRoles");
+            builder.Entity<IdentityUserLogin>().HasKey(i => i.UserId).ToTable("ApplicationUserLogins");
+            builder.Entity<IdentityRole>().ToTable("ApplicationRoles");
+            builder.Entity<IdentityUserClaim>().HasKey(i => i.UserId).ToTable("ApplicationUserClaims");
 
         }
     }
--- OnlineShop.Mode/Models/ApplicationUser.cs
+++ OnlineShop.Mode/Models/ApplicationUser.cs
@@ -1,5 +1,5 @@
-﻿//using Microsoft.AspNet.Identity;
-//using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,24 +8,24 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 namespace OnlineShop.Mode.Models
 {
-    //public class ApplicationUser : IdentityUser
-    //{
-    //    [MaxLength(256)]
-    //    public string FullName { set; get; }
+    public class ApplicationUser : IdentityUser
+    {
+        [MaxLength(256)]
+        public string FullName { set; get; }
 
-    //    [MaxLength(256)]
-    //    public string Address { set; get; }
+        [MaxLength(256)]
+        public string Address { set; get; }
 
-    //    public DateTime? BirthDay { set; get; }
+        public DateTime? BirthDay { set; get; }
 
-    //    public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
-    //    {
-    //        // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-    //        var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
-    //        // Add custom user claims here
-    //        return userIdentity;
-    //    }
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
 
-    //    public virtual IEnumerable<Order> Orders { set; get; }
-    //}
+        public virtual IEnumerable<Order> Orders { set; get; }
+    }
 }
